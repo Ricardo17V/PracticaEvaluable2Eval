6f329be3805702a097a20db4fcc21d260fd8aa7f
ricardo <ricardo@DESKTOP-KLL37QO>
2026-02-27 12:22:58 +0100

    Clase hija de la clase Base, llamada DronMaritimo en la que se implementa el atributo resistenciaSalinidad que es un número entero, en la que tenemos el constructor de DronMaritimo con sus atributos y otro vacio, con el método calcularTiempoRespuesta que te devuelve el tiempo

---
+++ Peval2Eval/src/drones/DronMaritimo.java
@@ -0,0 +1,32 @@
+package drones;
+
+public class DronMaritimo extends Base {
+    private int resistenciaSalinidad;
+
+    public DronMaritimo(int id, int autonomia, double velocidad, boolean operativo, int resistenciaSalinidad) {
+        super(id, autonomia, velocidad, operativo);
+        this.resistenciaSalinidad = resistenciaSalinidad;
+    }
+    public DronMaritimo() {
+    	
+    }
+    @Override
+    public double calcularTiempoRespuesta(double distancia) throws Exception {
+        comprobarEstado();
+        if (distancia < 0) {
+            throw new Exception("la distancia no puede ser negativa");
+        }
+
+        double tiempo = distancia / getVelocidadMax();
+
+        if (resistenciaSalinidad < 30) {
+            tiempo *= 1.20;
+        }
+
+        if (tiempo > getAutonomiaMax()) {
+            throw new Exception("el tiempo excede la autonomía disponible");
+        }
+
+        return tiempo;
+    }
+}
